5638cf84c9825ac24519d3046d5bd5d2331ce879
jsphantomdev <96658288+gphillips8@users.noreply.github.com>
2021-12-28 17:21:17 -0500

    Updates

---
+++ precache-manifest.930810930891a719f40e6472f3ec4311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1957fcf5c41dd4cb11e3c3ff1aedc018",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "7753e3da49bd9f8ddb84",
+    "url": "/react-monsters/static/css/main.2af87e3a.chunk.css"
+  },
+  {
+    "revision": "b260d2dd141c0c27de97",
+    "url": "/react-monsters/static/js/2.c877fdbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.c877fdbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7753e3da49bd9f8ddb84",
+    "url": "/react-monsters/static/js/main.a8faf8ea.chunk.js"
+  },
+  {
+    "revision": "a488bef04a42031c06ca",
+    "url": "/react-monsters/static/js/runtime-main.97ee3d9b.js"
+  }
+]);
